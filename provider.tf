a8962dbe1897ab85b15ff17ebfeaf2cfd01fab25
Reddeiah <reddeiah18g@gmail.com>
2026-09-09 12:53:28 +0530

    adding file

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-86s-dev"
+    bucket = "remote-state-86s-dev1"
     key    = "roboshop-dev-workstation"
     region = "us-east-1"
     use_lockfile = true

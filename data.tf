2dcd3057548583e6b857d611a1b95096af516445
Reddeiah <reddeiah18g@gmail.com>
2026-09-09 12:51:31 +0530

    adding file

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "joindevops" {
-    owners           = ["973714476881"]
+    owners           = ["459141725630"]
     most_recent      = true
     
     filter {
--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-    ami_id = data.aws_ami.joindevops.id
+    ami_id = data.aws_ami.Likith.id
     common_tags = {
         Project = var.project_name
         Environment = var.environment
